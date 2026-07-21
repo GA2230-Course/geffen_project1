39ca7e0d4f6c758d804071109a6c47b5dba42a35
Your Name <your_real_email@gmail.com>
2026-07-21 15:27:54 +0300

    proj3

--- src/ledsystem/LedController.java
+++ src/ledsystem/LedController.java
@@ -1,17 +1,39 @@
 package ledsystem;
-class LedController{
+import ledsystem.utils.StopWatch;
+import java.awt.*;
+
+class LedController {
     private final LedStrip strip;
     private Animation animation;
+    private final StopWatch sw = new StopWatch();
 
     public LedController (int stripLength)  {
         this.strip = new LedStrip(stripLength);
+        this.sw.start();
     }
+
     public void addAnimation(Animation animation){
-        this.animation=animation;
+        this.animation = animation;
     }
+
     public void play(){
-        if (this.animation!= null){
-            this.animation.apply(strip);
+        boolean isColorA = true;
+        double targetTime = 2.0;
+
+        while (this.animation != null){
+            if (sw.get() >= targetTime) {
+                if (isColorA) {
+                    this.animation = new ledsystem.SolidAnimation(Color.RED);
+                } else {
+                    this.animation = new ledsystem.SolidAnimation(Color.GREEN);
+                }
+
+                this.animation.apply(strip);
+                isColorA = !isColorA;
+                targetTime += 2.0;
+            }
         }
     }
 }
+
+
