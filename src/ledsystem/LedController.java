ebf4a34cd950d651f6c6fe0a21412dc8734cd06c
Your Name <your_real_email@gmail.com>
2026-07-21 14:13:12 +0300

    finished

--- src/ledsystem/LedController.java
+++ src/ledsystem/LedController.java
@@ -1,6 +1,17 @@
 package ledsystem;
-import java.awt.Color;
-
 class LedController{
     private final LedStrip strip;
+    private Animation animation;
+
+    public LedController (int stripLength)  {
+        this.strip = new LedStrip(stripLength);
+    }
+    public void addAnimation(Animation animation){
+        this.animation=animation;
+    }
+    public void play(){
+        if (this.animation!= null){
+            this.animation.apply(strip);
+        }
+    }
 }
