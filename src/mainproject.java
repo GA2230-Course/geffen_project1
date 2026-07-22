6d76e44892ceacb6c52ebe17b86e31b44369aa0f
Your Name <your_real_email@gmail.com>
2026-07-22 11:55:32 +0300

    proj1

--- src/mainproject.java
+++ src/mainproject.java
@@ -2,8 +2,10 @@ package ledsystem;
 
 import java.awt.Color;
 
-class Project1 {
+public class mainproject {
     public static void main(String[] args) {
+
+
         LedStrip myStrip = new LedStrip(10);
         ledsystem.SolidAnimation redAnimation = new ledsystem.SolidAnimation(Color.RED);
         redAnimation.apply(myStrip);
